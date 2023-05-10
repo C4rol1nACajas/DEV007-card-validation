ae7ddbe5677bc48870c047a857582572a8b44cbe
C4rol1nACajas <ccajasdelaguila@gmail.com>
2023-05-10 08:28:56 -0500

    carodonut

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+const inputNumber = document.getElementById("txtNumeroTarjeta");
+inputNumber.addEventListener("keypress", (e) => {
+  if (!onlyNumbers(e)) {
+    e.preventDefault();
+  }
+});
+const inputCvc = document.getElementById("txtCvc");
+inputCvc.addEventListener("keypress", (e) => {
+  if (!onlyNumbers(e)) {
+    e.preventDefault();
+  }
+});
+function onlyNumbers(e) {
+  const key = e.charCode;
+  return key >= 48 && key <= 57;
+}
+const formValidation = document.getElementById("formValidation");
+formValidation.addEventListener("submit", () => {
+  const x = validator.isValid(
+    document.getElementById("txtNumeroTarjeta").value
+  );
+  const mascara = validator.maskify(
+    document.getElementById("txtNumeroTarjeta").value
+  );
+  if (x === true) {
+    alert("tu tarjeta es valida " + mascara);
+  } else {
+    alert("tu tarjeta no es valida " + mascara);
+  }
+});
+// Obtener el elemento input
+const input = document.getElementById('txtExpDate');
+input.addEventListener('input', (event) => {
+  let inputValue = event.target.value;
+  inputValue = inputValue.replace(/\D/g,''); // remueve todo lo que no sea un dígito
+  inputValue = inputValue.substring(0,4); // limita el input a cuatro caracteres
+  inputValue = inputValue.replace(/(\d{2})(\d{2})/,'$1/$2'); // agrega la barra después del segundo dígito
+  event.target.value = inputValue;
+});
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,42 @@
 const validator = {
-  // ...
-};
+  isValid: function(creditCardNumber) {
+    //alert("Tu número de tarjeta es: "+creditCardNumber);
+    // Convierte el número en un array y lo invierte
+    const digits = creditCardNumber.toString().split('').map(Number).reverse();
 
+    // Variable para almacenar la suma de los dígitos
+    let sum = 0;
+
+    // Itera por cada dígito del número
+    for (let i = 0; i < digits.length; i++) {
+      // Si el índice es impar, duplica el dígito
+      if (i % 2 === 1) {
+        digits[i] *= 2;
+        // Si el doble es mayor que 9, resta 9
+        if (digits[i] > 9) {
+          digits[i] -= 9;
+        }
+      }
+      // Agrega el dígito a la suma
+      sum += digits[i];
+    }
+
+    // El número es válido si la suma es divisible por 10
+    if(sum % 10 === 0) {
+      return true
+    }
+    else {
+      return false
+    }
+  },
+  maskify: function(creditCardNumber) {
+    // Si el número es menor o igual a 4 caracteres, no lo modifica
+    if (creditCardNumber.length <= 4) {
+      return creditCardNumber;
+    }
+    // Obtiene los últimos 4 caracteres del número
+    const lastFourDigits = creditCardNumber.slice(-4);
+    /* Genera una cadena de caracteres del mismo largo que el número original,
+     pero con los últimos 4 caracteres reemplazados por # */
+    return '#'.repeat(creditCardNumber.length - 4) + lastFourDigits; }}  
 export default validator;
