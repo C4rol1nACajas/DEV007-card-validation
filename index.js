59da43b51043f4466ae5f6ba63ec191dce86b62d
C4rol1nACajas <ccajasdelaguila@gmail.com>
2023-05-10 20:32:09 -0500

    Updates

--- index.js
+++ index.js
@@ -18,24 +18,24 @@ function onlyNumbers(e) {
 }
 const formValidation = document.getElementById("formValidation");
 formValidation.addEventListener("submit", () => {
-  const x = validator.isValid(
+  const validacion = validator.isValid(
     document.getElementById("txtNumeroTarjeta").value
   );
   const mascara = validator.maskify(
     document.getElementById("txtNumeroTarjeta").value
   );
-  if (x === true) {
+  if (validacion === true) {
     alert("tu tarjeta es valida " + mascara);
   } else {
     alert("tu tarjeta no es valida " + mascara);
   }
 });
 // Obtener el elemento input
-const input = document.getElementById('txtExpDate');
-input.addEventListener('input', (event) => {
+const input = document.getElementById("txtExpDate");
+input.addEventListener("input", (event) => {
   let inputValue = event.target.value;
-  inputValue = inputValue.replace(/\D/g,''); // remueve todo lo que no sea un dígito
-  inputValue = inputValue.substring(0,4); // limita el input a cuatro caracteres
-  inputValue = inputValue.replace(/(\d{2})(\d{2})/,'$1/$2'); // agrega la barra después del segundo dígito
-  event.target.value = inputValue;
+  inputValue = inputValue.replace(/\D/g, ""); // remueve todo lo que no sea un dígito
+  inputValue = inputValue.substring(0, 4); // limita el input a cuatro caracteres
+  inputValue = inputValue.replace(/(\d{2})(\d{2})/, "$1/$2"); // agrega la barra después del segundo dígito
+  event.target.value = inputValue;
 });
